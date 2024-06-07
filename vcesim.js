0f79ff9834a6cd766fe1476f97752096843208ad
darkzero84 <darkzero84@gmail.com>
2024-06-07 17:43:54 +0700

    Update vcesim.js

--- vcesim.js
+++ vcesim.js
@@ -2,8 +2,8 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj = {
-  "result": 2,
-  "data": "test"
+	"result": 1,
+	"errormsg": "No active products available"
 }
   
 body = JSON.stringify(obj);
