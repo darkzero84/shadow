4a0c31670129975ed84bdf143616ebbc5fd2a947
darkzero84 <darkzero84@gmail.com>
2025-01-07 14:43:49 +0700

    Update vplusgo_open.js

--- vplusgo_open.js
+++ vplusgo_open.js
@@ -2,10 +2,26 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj = {
-	"data": {},
+	"data": {
+		"id": 1009,
+		"username": "snowangelvn2020@gmail.com",
+		"active": 1,
+		"product_id": "vplus_app_product_business",
+		"product_name": "Business",
+		"license": "6 months",
+		"days": 180,
+		"expired_date": null,
+		"number_openfile": 3,
+		"number_opened": 3,
+		"num_of_file": 3,
+		"num_of_file_type": "day",
+		"number_device": 3,
+		"number_device_login": 0,
+		"token": null
+	}
 	"status": 200,
-	"success": 2,
-	"message": "Hello World!!!"
+	"success": 1,
+	"message": ""
 }
   
 body = JSON.stringify(obj);
